d69c9457cfd54a5341dfc872d70d82b1a1890118
Вишневский Влад <vishnevskii_vlad@mail.ru>
2025-04-15 13:42:27 +0300

    add posts

--- src/components/post/post.component.tsx
+++ src/components/post/post.component.tsx
@@ -1,9 +1,14 @@
+import styles from './post.module.scss';
+
 export const Post = () => {
   return (
-    <div>
+    <div className={styles.post}>
       <img src="https://cdn2.thecatapi.com/images/e7v.jpg" alt="" />
-      <h3>Морские котики и львы: новые связи в мире морских млекопитающих</h3>
-      <p>
+      <h3 className={styles.post_title}>
+        Морские котики и львы: новые связи в мире морских млекопитающих
+        <span className={styles.post_date}>30.02.2024</span>
+      </h3>
+      <p className={styles.post_text}>
         Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые
         модификации, например, юмористические вставки или слова, которые даже отдалённо не
         напоминают латынь. Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите
